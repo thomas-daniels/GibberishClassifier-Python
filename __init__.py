35620068869bef9edda073ee3f6b807a7062ed80
ProgramFOX <programfox@hotmail.be>
2015-04-08 11:36:38 +0200

    Initial commit - first version

---
+++ __init__.py
@@ -0,0 +1,5 @@
+"""
+Gibberish Classification package
+Author: ProgramFOX
+Licensed under CPOL v1.02: http://www.codeproject.com/info/cpol10.aspx
+"""
---
+++ gibberishclassifier.py
@@ -0,0 +1,69 @@
+"""
+Gibberish Classifier
+Author: ProgramFOX
+Licensed under CPOL v1.02: http://www.codeproject.com/info/cpol10.aspx
+"""
+
+from __future__ import division
+import re
+import math
+
+
+def split_in_chunks(text, chunk_size):
+    chunks = []
+    for i in range(0, len(text), chunk_size):
+        chunks.append(text[i:i+chunk_size])
+    if len(chunks) > 1 and len(chunks[-1]) < 10:
+        chunks[-2] += chunks[-1]
+        chunks.pop(-1)
+    return chunks
+
+
+def unique_chars_per_chunk_percentage(text, chunk_size):
+    chunks = split_in_chunks(text, chunk_size)
+    unique_chars_percentages = []
+    for chunk in chunks:
+        total = len(chunk)
+        unique = len(set(chunk))
+        unique_chars_percentages.append(unique / total)
+    return sum(unique_chars_percentages) / len(unique_chars_percentages) * 100
+
+
+def vowels_percentage(text):
+    vowels = 0
+    total = 0
+    for c in text:
+        if not c.isalpha():
+            continue
+        total += 1
+        if c in "aeiouAEIOU":
+            vowels += 1
+    return vowels / total * 100
+
+
+def word_to_char_ratio(text):
+    chars = len(text)
+    words = len([x for x in re.split(r"[\W_]", text) if x.strip() != ""])
+    return words / chars * 100
+
+
+def deviation_score(percentage, lower_bound, upper_bound):
+    if percentage < lower_bound:
+        return math.log(lower_bound - percentage, lower_bound) * 100
+    elif percentage > upper_bound:
+        return math.log(percentage - upper_bound, 100 - upper_bound) * 100
+    else:
+        return 0
+
+
+def classify(text):
+    ucpcp = unique_chars_per_chunk_percentage(text, 35)
+    vp = vowels_percentage(text)
+    wtcr = word_to_char_ratio(text)
+
+    ucpcp_dev = max(deviation_score(ucpcp, 45, 50), 1)
+    vp_dev = max(deviation_score(vp, 35, 45), 1)
+    wtcr_dev = max(deviation_score(wtcr, 15, 20), 1)
+
+    return max((math.log10(ucpcp_dev) + math.log10(vp_dev) +
+                math.log10(wtcr_dev)) / 6 * 100, 1)
